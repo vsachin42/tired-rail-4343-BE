1789cb9483ddee820a0020496648f1242119dae4
Sachin <soniji7269@gmail.com>
2023-07-23 15:58:04 +0530

    BE

--- Routes/user.route.js
+++ Routes/user.route.js
@@ -55,18 +55,6 @@ userRouter.post("/login", async(req,res) => {
 })
 
 
-userRouter.get("/", async(req,res) =>  {
-   try{
-    const users = await userModel.find();
-    res.status(200).json(users);
-   }catch(err){
-    res.status(404).json({error:err})
-   }
-})
-
-
-
-
 module.exports = {
     userRouter
 }
